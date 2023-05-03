80d9ce0f189b450b76408fea7316a29e75b4b4bd
Terockk <106120548+Terockk@users.noreply.github.com>
2023-05-03 19:16:05 -0300

    Fmod Project and Audio scripts/prefabs

---
+++ Assets/_GAME/_Scripts/Audio/AmbienceParameter.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParameterSetByName : MonoBehaviour
+{
+    FMOD.Studio.EventInstance Ambience;
+
+    private void Start()
+    {
+        Ambience = FMODUnity.RuntimeManager.CreateInstance("event:/Music/Ambience");
+        Ambience.start();
+    }
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "PLAYER")
+            Ambience.setParameterByName("Ambience Fade", 1f);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "PLAYER")
+            Ambience.setParameterByName("Ambience Fade", 0f);
+    }*/
+
+    /*private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.TryGetComponent(out PlayerController player))
+        {
+            Ambience.setParameterByName("Ambience Fade", 1f);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.TryGetComponent(out PlayerController player))
+        {
+            Ambience.setParameterByName("Ambience Fade", 0f);
+        }
+    }*/ 
+
+}
---
+++ Assets/_GAME/_Scripts/Audio/FootstepPlayer.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class FootstepPlayer : MonoBehaviour
+{
+    private float distance = 0.4f;
+    private float Material;
+
+    [SerializeField] private bool debug;
+
+
+
+    void PlayMeleeEvent(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
+    }
+
+    void FixedUpdate()
+    {
+        MaterialCheck();
+
+        if(debug)
+            Debug.DrawRay(transform.position, Vector2.down * distance, Color.blue);
+    }
+
+    void MaterialCheck()
+    {
+        RaycastHit2D hit;
+        hit = Physics2D.Raycast(transform.position, Vector2.down, distance, 1 << 3);           //Layer
+
+        if (hit.collider)
+
+        {
+            if (hit.collider.tag == "ground")
+            {
+                Material = 1f;
+            }
+            if (hit.collider.tag == "Escada")
+            {
+                Material = 2f;
+            }
+            
+
+        }
+    }
+
+    void PlayFootstepsEvent(string path)
+    {
+            FMOD.Studio.EventInstance Footsteps = FMODUnity.RuntimeManager.CreateInstance(path);
+            Footsteps.setParameterByName("Material", Material);
+            Footsteps.start();
+            Footsteps.release();
+    }
+
+
+
+}
